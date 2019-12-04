656f4c16717f4dafa5af170027f0bf11f3c5f406
Hanyon Wu <han4wluc@gmail.com>
2019-12-04 10:44:04 +0800

    clean index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,3 @@
-
-// import { NextPage } from 'next'
-
-// import { observable } from "mobx"
-
-// // class Todo {
-// //     id = Math.random()
-// //     @observable title = ""
-// //     @observable finished = false
-// // }
-
-
-// const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-//     <h1>Hello world! - user agent: {userAgent}</h1>
-// )
-
-// Home.getInitialProps = async ({ req }) => {
-//     const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
-//     return { userAgent };
-// };
-
-// export default Home
-
 import React from 'react'
 import Todo from '../components/Todo'
 
