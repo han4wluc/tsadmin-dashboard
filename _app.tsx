889b06971b7bcca22069fa2320bd2f055ae7ee94
Hanyon Wu <han4wluc@gmail.com>
2019-12-04 11:18:04 +0800

    setup mocha

---
+++ _app.tsx
@@ -0,0 +1,39 @@
+// import App from 'next/app'
+// import React from 'react'
+// import { fetchInitialStoreState, Store } from '../store'
+// import { TodoStore } from '../stores/TodoStore'
+// import { Provider } from 'mobx-react'
+
+// class MyMobxApp extends App {
+//   state = {
+//     store: new Store(),
+//     todoStore: new TodoStore()
+//   }
+
+//   // Fetching serialized(JSON) store state
+//   static async getInitialProps(appContext: any) {
+//     const appProps = await App.getInitialProps(appContext)
+//     const initialStoreState = await fetchInitialStoreState()
+
+//     return {
+//       ...appProps,
+//       initialStoreState,
+//     }
+//   }
+
+//   // Hydrate serialized state to store
+//   static getDerivedStateFromProps(props: any, state: any) {
+//     state.store.hydrate(props.initialStoreState)
+//     return state
+//   }
+
+//   render() {
+//     const { Component, pageProps } = this.props
+//     return (
+//       <Provider store={this.state.store} todoStore={this.state.todoStore} >
+//         <Component {...pageProps} />
+//       </Provider>
+//     )
+//   }
+// }
+// export default MyMobxApp
---
+++ tests/test1.spec.ts
@@ -0,0 +1,9 @@
+
+import { assert } from 'chai'
+import 'mocha';
+
+describe('extractFilter', () => {
+    it('should extract single condition', () => {
+        assert.deepEqual(1, 1)
+    })
+})
