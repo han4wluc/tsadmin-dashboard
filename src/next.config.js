7ff8a96c12ad640bfd46f0101c8b68a8ab114a4a
Hanyon Wu <han4wluc@gmail.com>
2020-01-03 21:13:38 +0800

    next build export

--- src/next.config.js
+++ src/next.config.js
@@ -1,27 +1,33 @@
 /* eslint-disable */
-const withCss = require('@zeit/next-css')
+const withCss = require('@zeit/next-css');
 const path = require('path');
 
 module.exports = withCss({
+  exportTrailingSlash: true,
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' }
+    };
+  },
   webpack: (config, { isServer }) => {
-
     config.resolve.alias['~'] = path.join(path.resolve(__dirname));
 
+
     if (isServer) {
-      const antStyles = /antd\/.*?\/style\/css.*?/
-      const origExternals = [...config.externals]
+      const antStyles = /antd\/.*?\/style\/css.*?/;
+      const origExternals = [...config.externals];
       config.externals = [
         (context, request, callback) => {
-          if (request.match(antStyles)) return callback()
+          if (request.match(antStyles)) return callback();
           if (typeof origExternals[0] === 'function') {
-            origExternals[0](context, request, callback)
+            origExternals[0](context, request, callback);
           } else {
-            callback()
+            callback();
           }
         },
         ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-      ]
+      ];
     }
-    return config
+    return config;
   },
-})
+});
